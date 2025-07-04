9bce746279aec848a8b7ddb1b6990917decb6cb2
MironHasen <miron.hesan@gmail.com>
2025-07-04 23:42:20 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+// const actionButtons = document.getElementById("sticky-buttons");
+// const originalOffset = actionButtons.offsetTop;
+// 
+// window.addEventListener("scroll", () => {
+  // if (window.scrollY >= originalOffset - 60) {
+    // actionButtons.classList.add("fixed");
+  // } else {
+    // actionButtons.classList.remove("fixed");
+  // }
+// });
+// 
